1a67330d1e50564659d58c0e54f5ed1430ab4ee2
Fernando Figueiredo Drummond Bittencourt <fernandodbittencourt@hotmail.com>
2018-03-18 14:58:01 -0300

    Lab 1 Part 2 Solution.

---
+++ lab-1/lab1_Part2/src/main/java/com/example/lab1/TeamController.java
@@ -0,0 +1,31 @@
+package com.example.lab1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeamController {
+	
+	@GetMapping("/teams")
+	public List<Team> getTeams(){
+		List<Team> list = new ArrayList<>();
+
+	  	Team team = new Team();
+	  	team.setId(0l);
+	  	team.setLocation("Harlem");
+	  	team.setName("Globetrotters");
+	  	team.setMascot("Big G");
+	  	list.add(team);
+	  	
+	  	team = new Team();
+	  	team.setId(1l);
+	  	team.setLocation("Washington");
+	  	team.setName("Generals");
+	  	list.add(team);
+	  	
+	  	return list;
+	}
+}
