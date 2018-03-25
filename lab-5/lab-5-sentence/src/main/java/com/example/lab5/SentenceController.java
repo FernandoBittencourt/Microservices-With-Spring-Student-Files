28c053442f0b613d50f930c3b7a3acca8abbbccf
Fernando Figueiredo Drummond Bittencourt <fernandodbittencourt@hotmail.com>
2018-03-25 18:14:18 -0300

    Lab 5 Solution.

--- lab-5/lab-5-sentence/src/main/java/com/example/lab5/SentenceController.java
+++ lab-5/lab-5-sentence/src/main/java/com/example/lab5/SentenceController.java
@@ -1,11 +1,6 @@
-package com.example.lab4;
-
-import java.net.URI;
-import java.util.List;
+package com.example.lab5;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class SentenceController {
 
 	@Autowired
-	DiscoveryClient client;
+	RestTemplate template;
 
 	@GetMapping("/sentence")
 	public @ResponseBody String getSentence() {
@@ -24,13 +19,6 @@ public class SentenceController {
 	}
 
 	public String getWord(String service) {
-		List<ServiceInstance> list = client.getInstances(service);
-		if (list != null && list.size() > 0) {
-			URI uri = list.get(0).getUri();
-			if (uri != null) {
-				return (new RestTemplate()).getForObject(uri, String.class);
-			}
-		}
-		return null;
+		return template.getForObject("http://" + service, String.class);
 	}
 }
